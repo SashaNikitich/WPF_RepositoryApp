fb10565ea80535e38e53879b1a7f65cb31bb6cb0
SashaNikitich <sashanikitich@gmail.com>
2024-07-20 11:46:50 +0300

    Register_test

--- WpfTest/obj/Debug/net8.0-windows/WpfTest.AssemblyInfo.cs
+++ WpfTest/obj/Debug/net8.0-windows/WpfTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a318a3580b64c5cb7bc3f96fc3d670a4d5b4dbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a66ed028f3779ea36f1d0b2ce936f1a92fa8438f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
